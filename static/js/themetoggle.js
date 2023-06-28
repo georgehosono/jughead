63283e55c5291c69d8f8639fbb909fd8bd017a7a
George Hosono <georgehosono@gmail.com>
2023-06-27 23:18:58 -0700

    :sparkles: Updated colorscheme and UI

--- static/js/themetoggle.js
+++ static/js/themetoggle.js
@@ -1,23 +1,40 @@
-function setTheme(mode) {
-    localStorage.setItem("theme-storage", mode);
-    if (mode === "dark") {
-        document.getElementById("darkModeStyle").disabled=false;
-        document.getElementById("dark-mode-toggle").innerHTML = "<i data-feather=\"sun\"></i>";
-        feather.replace()
-    } else if (mode === "light") {
-        document.getElementById("darkModeStyle").disabled=true;
-        document.getElementById("dark-mode-toggle").innerHTML = "<i data-feather=\"moon\"></i>";
-        feather.replace()
-    }
+const setTheme = (theme) => {
+  if (theme !== 'light' && theme !== 'dark') {
+    throw new Error(`Invalid theme value for setTheme: '${theme}'`)
+  }
+
+  const root = document.documentElement
+  const icon = document.querySelector('#dark-mode-icon')
+  if (theme === 'dark') {
+    icon.dataset.feather = 'sun'
+  } else if (theme === 'light') {
+    icon.dataset.feather = 'moon'
+  }
+  root.dataset.theme = theme
+  window.localStorage.setItem('theme', theme)
+  feather.replace()
 }
 
-function toggleTheme() {
-    if (localStorage.getItem("theme-storage") === "light") {
-        setTheme("dark");
-    } else if (localStorage.getItem("theme-storage") === "dark") {
-        setTheme("light");
-    }
-}
+const toggle = document.querySelector('#dark-mode-toggle')
+toggle.addEventListener('click', (e) => {
+  e.preventDefault()
+  let theme = window.localStorage.getItem('theme')
+  if (theme == 'light') {
+    theme = 'dark'
+  } else if (theme === 'dark') {
+    theme = 'light'
+  } else {
+    throw new Error(`Invalid theme in localStorage: '${theme}'`)
+  }
+  window.localStorage.setItem('theme', theme)
+  setTheme(theme)
+})
 
-var savedTheme = localStorage.getItem("theme-storage") || "light";
-setTheme(savedTheme);
+if (!window.localStorage.getItem('theme')) {
+  if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+    window.localStorage.setItem('theme', 'light')
+  } else {
+    window.localStorage.setItem('theme', 'dark')
+  }
+}
+setTheme(window.localStorage.getItem('theme'))
